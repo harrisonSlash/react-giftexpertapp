dd50e573c9ebee685206eaf3765944c07b6777e5
Emmanuel Delgado <cheto_roroazul@hotmail.com>
2020-07-07 18:21:54 -0500

    Added docs

---
+++ docs/precache-manifest.2172c9c5f973c7fa0dd2578f8d9ad5f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a08e8fbcb5d265c2635dfb01d1efdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d51024e7f96db27df576",
+    "url": "/static/css/main.e357b66e.chunk.css"
+  },
+  {
+    "revision": "2c6d6d15c50165bbb9f0",
+    "url": "/static/js/2.c2993900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2993900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51024e7f96db27df576",
+    "url": "/static/js/main.8bc78b62.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
